baed792e8f7d697468c36db40ceff083b59cb808
valetronika <valentina2wo@gmail.com>
2023-05-12 21:14:25 +0200

    add change

--- hw_function_4.05.js
+++ hw_function_4.05.js
@@ -4,7 +4,7 @@
 let checkString = function (arg) {
     console.log(arg[0]);
 };
-// checkString('dog')
+checkString('dog')
 
 // Напишите функцию checkMiddleValue(num1,num2), которая получает в качестве аргумента два числовых значения.
 // Функция должна вернуть их среднее значение.
@@ -13,7 +13,7 @@ let checkString = function (arg) {
 let checkMiddleValue = function (num1, num2) {
     return (+(num1 + num2) / 2).toFixed(2);  // возращает строку ставим + сперели
 };
-// console.log(checkMiddleValue(56, 45));
+console.log(checkMiddleValue(56, 45));
 
 //2  Напишите функцию changeValue(arg1, arg2), которая принимает два аргумента (булевый тип, число).
 // Функция, в зависимости от первого аргумента (булевого типа) должна изменить число по следующему правилу:
@@ -23,7 +23,7 @@ let changeValue = (arg1, arg2) => (arg1 ? Math.pow(arg2, 2) : Math.sqrt(arg2));
 
 // let changeValue = (arg1, arg2) => (arg1 ? arg2**2 : arg2**0.5);
 
-// console.log(changeValue(false, 25));
+console.log(changeValue(false, 25));
 
 // Напишите функцию countString(arg), которая принимает в качестве аргумента строковый
 // тип и возвращает количество букв. Функция должна игнорировать буквы: а, о, у, с
@@ -66,22 +66,23 @@ console.log(countString("Cucfdhrtey"));
 // Функция должна построить последовательность строк в консоле, 
 // количество которых зависит от аргумента 
 
-// function createQuence(num){
-//     let a = '*'
-//     for (let i = 0; i < num; i++) {
-//         // console.log(a);
-//         a = a + '*';
-//     }
-// }
-function createQuence(num) {
-    let result = '';
-
-    for (let i = 1; i <= num; i++) {
-      for (let j = 1; j <= i; j++) {
-        result += '*';
-      }
-      result += '\n';
+function createQuence(num){
+    let a = '*'
+    for (let i = 0; i < num; i++) {
+        // console.log(a);
+        a = a + '*';
     }
-    console.log(result);
-  }
-createQuence(5)
+}
+
+// function createQuence(num) {  не мое решение, оставила тут для примера
+//     let result = '';
+
+//     for (let i = 1; i <= num; i++) {
+//       for (let j = 1; j <= i; j++) {
+//         result += '*';
+//       }
+//       result += '\n';
+//     }
+//     console.log(result);
+//   }
+// createQuence(5)
