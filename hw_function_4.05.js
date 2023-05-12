1acdbb4cd96a06782f61435fc0cf361b25a3d169
valetronika <valentina2wo@gmail.com>
2023-05-12 21:09:25 +0200

    add hw

--- hw_function_4.05.js
+++ hw_function_4.05.js
@@ -11,7 +11,7 @@ let checkString = function (arg) {
 // * Доп задание. доработайте функцию таким образом, чтобы выводимое значение округлялось до десятой части
 
 let checkMiddleValue = function (num1, num2) {
-    return ((num1 + num2) / 2).toFixed(2);
+    return (+(num1 + num2) / 2).toFixed(2);  // возращает строку ставим + сперели
 };
 // console.log(checkMiddleValue(56, 45));
 
@@ -30,32 +30,58 @@ let changeValue = (arg1, arg2) => (arg1 ? Math.pow(arg2, 2) : Math.sqrt(arg2));
 // * Доп задание. доработайте функцию таким образом, чтобы условие включало
 // верхний и нижний регистр (А а, О о, У у, С с)
 
+// let countString = (arg) => {
+//     let a = [];
+//     for (let i = 0; i < arg.length; i++) {
+//         if (
+//             arg[i].toLowerCase() != "a" &&
+//             arg[i].toLowerCase() != "o" &&
+//             arg[i].toLowerCase() != "y" &&
+//             arg[i].toLowerCase() != "c"
+//         )
+//             a.push(arg[i]);
+//     }
+//     return a.length;
+// };
+
 let countString = (arg) => {
     let a = [];
     for (let i = 0; i < arg.length; i++) {
+        let b= arg[i].toLowerCase()
         if (
-            arg[i].toLowerCase() != "a" &&
-            arg[i].toLowerCase() != "o" &&
-            arg[i].toLowerCase() != "y" &&
-            arg[i].toLowerCase() != "c"
+            b != "a" &&
+            b != "o" &&
+            b != "y" &&
+            b != "c"
         )
             a.push(arg[i]);
     }
     return a.length;
 };
 
-// console.log(countString("Cucfdhrtey"));
+
+console.log(countString("Cucfdhrtey"));
 
 // Напишите функцию createQuence(num), которая получает в качестве аргумента числовое значение. 
 // Функция должна построить последовательность строк в консоле, 
 // количество которых зависит от аргумента 
 
-function createQuence(num){
-    let a = '*'
-    for (let i = 0; i < num; i++) {
-        console.log(a);
-        a = a + '*';
-    }
-}
+// function createQuence(num){
+//     let a = '*'
+//     for (let i = 0; i < num; i++) {
+//         // console.log(a);
+//         a = a + '*';
+//     }
+// }
+function createQuence(num) {
+    let result = '';
 
-// createQuence(5)
+    for (let i = 1; i <= num; i++) {
+      for (let j = 1; j <= i; j++) {
+        result += '*';
+      }
+      result += '\n';
+    }
+    console.log(result);
+  }
+createQuence(5)
