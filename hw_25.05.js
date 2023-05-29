e6da0e535e0f4cfb443e6c04c736fe15a3dd6bbb
valetronika <valentina2wo@gmail.com>
2023-05-29 18:47:26 +0200

    add hm 25.05

--- hw_25.05.js
+++ hw_25.05.js
@@ -2,29 +2,56 @@
 
 // Вот задачи для решения, предварительно скажу что есть моменты которые надо прогуглить, несколько здача являются очень частыми ворпосами на собесах
 
-const numbers = [15, 15, -6, 95, 7];
+const numbers = [15, -6, 7, -6, 15];
+const strings = ["array", "with", "strings"];
+
 // Найдите сумму всех элементов в массиве.
 const sum_numbers = numbers.reduce((acc, val) => acc + val, 0);
 // console.log(sum_numbers);
+
 // Найдите наибольшее число в массиве.
 const big_number = numbers.reduce((acc, val) => (acc < val ? val : acc), 0);
 // console.log(big_number);
+
 // Найдите наименьшее число в массиве.
 const small_number = numbers.reduce((acc, val) => (acc > val ? val : acc), 0);
 // console.log(small_number);
+
 // Посчитайте количество отрицательных чисел в массиве.
 let count=0;
 numbers.forEach((el) => (el < 0 ? count++ : count));
 // console.log(count);
+
 // Проверьте, все ли элементы в массиве являются положительными числами.
-// console.log( numbers.every((el)=>el>=0));
+numbers.every((el)=>el>=0);
+
 // Отсортируйте массив в порядке возрастания.
 numbers.sort((a,b)=>a-b)
+
 // Отсортируйте массив в порядке убывания.
 numbers.sort((a,b)=> b-a)
 // console.log(numbers);
+
 // Извлеките все уникальные элементы из массива.
-let new_arr= numbers.filter(el=>)
-console.log(new_arr);
+const a =[... new Set(numbers)]
+// console.log(a);
+
 // Проверьте, является ли массив палиндромом (т.е. читается одинаково слева направо и справа налево).
+const numbers1 = [1,2,3,2,1];
+
+const palindrome = [...numbers1].reverse();
+const palindrome_check = () => {
+    let temp = 0;
+    for (let i = 0; i < palindrome.length; i++) {
+        if (palindrome[i] !== numbers1[i]) temp++;
+    }
+    return temp == 0;
+};
+console.log(palindrome_check());           //способ 1
+console.log(palindrome.join()===numbers1.join());//способ 2
+console.log(JSON.stringify(palindrome)===JSON.stringify(numbers1));//способ 3
 // Объедините два массива в один, чтобы получить новый массив, содержащий элементы обоих исходных массивов.
+
+const double_arr = strings.concat( numbers.map(String))
+
+// console.log(double_arr);
